5fe33a40f0266e37563eac031ee0e9e82e31f239
BT_1N3_14 <sample@bths.edu>
2023-09-22 09:30:23 -0400

    v3

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -5,5 +5,10 @@ public class CatRunner {
         Cat cat1 = new Cat("Penelope",12,15.75);
         cat1.printCatInfo();
         cat1.introduce();
+
+        Cat cat2 = new Cat("Simba",1,4.6);
+        cat2.introduce();
+        cat2.printCatInfo();
+
     }
 }
